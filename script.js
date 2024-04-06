e0652c8cd63cd59ac4f5ed8572da1df2e1657e02
WisnuDwiSeisno <wseisno37@gmail.com>
2024-04-06 19:40:52 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("ini video ILLIT");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini Audio ILLIT");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini Gambar ILLIT");
+});
